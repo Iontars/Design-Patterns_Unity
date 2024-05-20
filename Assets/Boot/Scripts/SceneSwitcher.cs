c01a0012c9829a616b5d12006f317beece976309
Граница миров <fmycry1@gmail.com>
2024-05-20 13:11:56 +0700

    Initial commit

---
+++ Assets/Boot/Scripts/SceneSwitcher.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 
+/// </summary>
+public class SceneSwitcher : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
